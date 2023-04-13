a06d5736d6f543d16b4fba391b66953429c9a76b
Marcin <gasiorek.mail@gmail.com>
2023-04-13 19:59:17 +0200

    create List and remove upper case and others

--- src/rankpage/ReaderRunner.java
+++ src/rankpage/ReaderRunner.java
@@ -2,22 +2,34 @@ package rankpage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ReaderRunner {
     public static void main(String[] args) {
         File file = new File("src\\rankpage\\text.txt");
+        String line;
+        List<String[]>list = new ArrayList<>();
+
         try{
             Scanner scanner = new Scanner(file);
             while(scanner.hasNextLine()){
-                String line = scanner.nextLine();
-                line = line.toLowerCase().replaceAll("ą","a").replaceAll("ż","z")
-                        .replaceAll("ź","z").replaceAll("ę","e")
-                        .replaceAll("ć","c").replaceAll("ś","s");
+                line = scanner.nextLine();
+                line= line.replaceAll("\\d+\\.","").replaceAll("ż","z")
+                        .replaceAll("ą","a").replaceAll("ę","e")
+                        .replaceAll(("ś"),"s").replaceAll("ć","c")
+                        .replaceAll("ź","z").replaceAll("ż","z").toLowerCase();
+                String[]sentence = line.split(" ");
+                list.add(sentence);
                 System.out.println(line);
+                System.out.println(Arrays.toString(sentence));
+
             }
         }catch (FileNotFoundException e){
             System.out.println("Nie znalazłem pliku");
         }
+
     }
 }
