f8b07440ddcb838b29a08f6aab855ff00f6ad2b5
Marcin <gasiorek.mail@gmail.com>
2023-04-13 20:35:36 +0200

    add functionality to find String search and count it.

--- src/rankpage/ReaderRunner.java
+++ src/rankpage/ReaderRunner.java
@@ -2,21 +2,19 @@ package rankpage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class ReaderRunner {
     public static void main(String[] args) {
         File file = new File("src\\rankpage\\text.txt");
-        String line;
+        String search = "pies";
         List<String[]>list = new ArrayList<>();
+        Map<String,Integer>wordsCount = new HashMap<>();
 
         try{
             Scanner scanner = new Scanner(file);
             while(scanner.hasNextLine()){
-                line = scanner.nextLine();
+               String line = scanner.nextLine();
                 line= line.replaceAll("\\d+\\.","").replaceAll("ż","z")
                         .replaceAll("ą","a").replaceAll("ę","e")
                         .replaceAll(("ś"),"s").replaceAll("ć","c")
@@ -30,6 +28,23 @@ public class ReaderRunner {
         }catch (FileNotFoundException e){
             System.out.println("Nie znalazłem pliku");
         }
+    for(String[] arr : list){
+        String key = Arrays.toString(arr);
+        Integer value=0;
+        for(String element : arr){
+            if(element.equals(search)){
+                value++;
+            }
+
+        }
+        wordsCount.put(key,value);
+    }
+    for(Map.Entry<String,Integer>entry : wordsCount.entrySet()){
+        String key = entry.getKey();
+        int value = entry.getValue();
+        if(value>0)
+        System.out.println(key + " : " + value);
+    }
 
     }
 }
